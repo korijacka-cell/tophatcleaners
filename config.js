22908195bba8395f39419fb4caca76187428b83a
korijacka-cell <korijacka@gmail.com>
2026-04-29 10:35:32 -0500

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// ============================================
-// Top Hat Cleaners — Supabase Config
-// ============================================
-
-window.SUPABASE_CONFIG = {
-  url: 'https://edhlrjawcpmtndjmqnqa.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkaGxyamF3Y3BtdG5kam1xbnFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0NTY4OTIsImV4cCI6MjA5MzAzMjg5Mn0.rmAJtwYFYV2TxvCMVomomemcfCPqBN2TqFuSdPPmhrM'
-};
